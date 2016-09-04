0af75b3d5d845f7ac081219b598fc22e31d73507
GBProd <contact@gb-prod.fr>
2016-09-04 21:53:12 +0200

    chore: bootstrap (#1)

---
+++ src/ElasticaSpecificationBundle/ElasticaSpecificationBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GBProd\ElasticaSpecificationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * ElasticaSpecificationBundle
+ *
+ * @author GBProd <contact@gb-prod.fr>
+ */
+class ElasticaSpecificationBundle extends Bundle
+{
+
+}
